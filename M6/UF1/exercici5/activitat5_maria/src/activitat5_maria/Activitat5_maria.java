f637adbfd6ccd9bafa2765d860efe4cc2fd77870
marianavarrovazquez <71724597+marianavarrovazquez@users.noreply.github.com>
2020-10-20 19:57:17 +0200

    Add files via upload

--- M6/UF1/exercici5/activitat5_maria/src/activitat5_maria/Activitat5_maria.java
+++ M6/UF1/exercici5/activitat5_maria/src/activitat5_maria/Activitat5_maria.java
@@ -1,16 +1,47 @@
 
 package activitat5_maria;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import static org.jcp.xml.dsig.internal.dom.DOMUtils.createElement;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /*
 *File: PersistenciFitxers.java
 *Author: Maria Navarro
-*Date: 18-10-2020
-*Description: Activitat 4 Practica UF1
+*Date: 20-10-2020
+*Description: Activitat 5 Practica UF1
 */
 
 public class Activitat5_maria {
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+
+    // per a carregar en memòria un arxiu xml
+        File arxiu = new File("alumnes.xml");
+        // Creo una instancia de DocumentBuilderFactory
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        // Creo un documentBuilder
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        // Obtengo el documento, a partir del XML
+        Document doc = dBuilder.parse(arxiu);
+
+        //per obtenir el node arrel
+        Element element = doc.getDocumentElement();
+        
+        //Per obtenir els nodes fill d’un node useu el mètode getChildNodes()
+        NodeList nodeChild =  doc.getChildNodes();        
+        
+        Scanner scan = new Scanner (System.in);
+       
+        
+
     }
     
 }
