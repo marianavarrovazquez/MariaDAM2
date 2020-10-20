b1306682e38f9438905c7c142be3f5ee1657f46f
marianavarrovazquez <71724597+marianavarrovazquez@users.noreply.github.com>
2020-10-20 18:09:18 +0200

    Add files via upload

---
+++ M6/UF1/exercici5/activitat5_maria/src/activitat5_maria/Activitat5_maria.java
@@ -0,0 +1,16 @@
+
+package activitat5_maria;
+
+/*
+*File: PersistenciFitxers.java
+*Author: Maria Navarro
+*Date: 18-10-2020
+*Description: Activitat 4 Practica UF1
+*/
+
+public class Activitat5_maria {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
