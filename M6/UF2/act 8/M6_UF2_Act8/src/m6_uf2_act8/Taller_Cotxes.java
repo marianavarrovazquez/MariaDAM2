8e38414c0feeb6818bd4ef1fbb2b3f11195e169a
marianavarrovazquez <71724597+marianavarrovazquez@users.noreply.github.com>
2020-11-22 23:36:19 +0100

    while vehicles

--- M6/UF2/act 8/M6_UF2_Act8/src/m6_uf2_act8/Taller_Cotxes.java
+++ M6/UF2/act 8/M6_UF2_Act8/src/m6_uf2_act8/Taller_Cotxes.java
@@ -15,7 +15,9 @@ public class Taller_Cotxes {
         
         //boolean per continuar els bucles
         boolean continua = true;
-        
+        //contador per continuar el bucle
+        int contador = 0;
+                
         //camp per propietari i vehicles
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         
@@ -46,7 +48,8 @@ public class Taller_Cotxes {
             System.out.println("Data de naixement del propietari: ");
             dNaixement = scan.next();
             //convertim el string a date
-            Date data = sdf.parse(dNaixement);
+            Date dataN = sdf.parse(dNaixement);
+            
             System.out.println("Telefon de contacte del propietari: ");
             tlf = scan.nextInt();
             
@@ -55,11 +58,56 @@ public class Taller_Cotxes {
             
             //creem la clae propietaris i indiquem els camps que te
             Propietaris propietari;
-            propietari = new Propietaris(nom, data, tlf, minusvalid);
+            propietari = new Propietaris(nom, dataN, tlf, minusvalid);
             
             em.persist(propietari);
             
-        }   
+            System.out.println("Vols introduir un altre propietari? \n 1 = SI || 2 = NO");
+            contador = scan.nextInt();
+            
+            if (contador == 1) {
+                continua = true;
+            } else {
+                continua = false;
+            }            
+        }
+        continua = true;
+        
+        System.out.println("Introdueix el/s vehicle/s");        
+        while(continua == true) {
+            System.out.println("Marca del vehicle: ");
+            marca = scan.nextLine();
+            
+            System.out.println("Data de fabricació del vehicle: ");
+            dFabricacio = scan.next();
+            //convertim el string a date
+            Date dataF = sdf.parse(dFabricacio);
+            
+            System.out.println("Numero de portes del vehicle: ");
+            portes = scan.nextInt();
+            
+            System.out.println("Si el vehicle consumeix diesel introdueix SI || introdueix NO en el cas de que utilitzi gasolina");
+            diesel = scan.nextBoolean();
+            
+            //creem la clae propietaris i indiquem els camps que te
+            Vehicles vehicle;
+            vehicle = new Vehicles(marca, dataF, portes, diesel);
+            
+            em.persist(vehicle);
+            
+            System.out.println("Vols introduir un altre vehicle? \n 1 = SI || 2 = NO");
+            contador = scan.nextInt();
+            
+            if (contador == 1) {
+                continua = true;
+            } else {
+                continua = false;
+            }            
+        }
+        
+        em.getTransaction().commit();
+        continua = true;
+        
         
     }
 }
