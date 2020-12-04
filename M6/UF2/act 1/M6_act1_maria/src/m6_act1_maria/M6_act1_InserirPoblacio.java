99d5dbddf37eeca3be01937972a58beb5f772185
maria <maria@LAPTOP-QQ1N2H54>
2020-12-04 03:02:45 +0100

    boto inserir poblacio codificat (error)

--- M6/UF2/act 1/M6_act1_maria/src/m6_act1_maria/M6_act1_InserirPoblacio.java
+++ M6/UF2/act 1/M6_act1_maria/src/m6_act1_maria/M6_act1_InserirPoblacio.java
@@ -5,6 +5,13 @@
  */
 package m6_act1_maria;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author maria
@@ -28,20 +35,68 @@ public class M6_act1_InserirPoblacio extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        LCPostal = new javax.swing.JLabel();
+        LPoblacio = new javax.swing.JLabel();
+        tfCPostal = new javax.swing.JTextField();
+        tfPoblacio = new javax.swing.JTextField();
+        BInsereix = new javax.swing.JButton();
+        BTornar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(203, 167, 120));
 
+        LCPostal.setText("Codi Postal: ");
+
+        LPoblacio.setText("Nom Poblacio:");
+
+        BInsereix.setText("Insereix");
+        BInsereix.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BInsereixActionPerformed(evt);
+            }
+        });
+
+        BTornar.setText("Tornar");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 344, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(139, Short.MAX_VALUE)
+                .addComponent(BInsereix)
+                .addGap(137, 137, 137))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BTornar)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(LCPostal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(LPoblacio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(tfPoblacio, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)
+                            .addComponent(tfCPostal))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 266, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LCPostal)
+                    .addComponent(tfCPostal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(27, 27, 27)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LPoblacio)
+                    .addComponent(tfPoblacio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(32, 32, 32)
+                .addComponent(BInsereix)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
+                .addComponent(BTornar)
+                .addGap(23, 23, 23))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -58,6 +113,41 @@ public class M6_act1_InserirPoblacio extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void BInsereixActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BInsereixActionPerformed
+        // TODO add your handling code here:
+        ResultSet comparar;
+        Statement stmt = null;
+        Statement stmtInsert = null; 
+        M6_act1_maria connect  = new M6_act1_maria();
+        
+        if (!tfCPostal.getText().equals("") && !tfPoblacio.getText().equals("")) {
+            try {
+                connect .conexioJDBC();
+                stmt = connect.conexioJDBC().createStatement();
+                comparar = stmt.executeQuery("SELECT codipostal FROM poblacions WHERE codipostal = '" + tfCPostal.getText() + "'");
+                
+                if(comparar.next()){
+                    JOptionPane.showMessageDialog(null, "Aquest codi postal ja ha estat registrat");
+                } else {
+                    stmtInsert = connect.conexioJDBC().createStatement();
+                    stmtInsert.execute("INSERT INTO poblacions VALUES (" + tfCPostal.getText() + ",'" + tfPoblacio.getText() + ")");
+                }
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(M6_act1_InserirAlumne.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(M6_act1_InserirPoblacio.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Falten camps per omplir");
+        }
+        
+    }//GEN-LAST:event_BInsereixActionPerformed
+
+    public void buidar(){
+        tfCPostal.setText("");
+        tfPoblacio.setText("");
+    }
     /**
      * @param args the command line arguments
      */
@@ -94,6 +184,12 @@ public class M6_act1_InserirPoblacio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BInsereix;
+    private javax.swing.JButton BTornar;
+    private javax.swing.JLabel LCPostal;
+    private javax.swing.JLabel LPoblacio;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField tfCPostal;
+    private javax.swing.JTextField tfPoblacio;
     // End of variables declaration//GEN-END:variables
 }
