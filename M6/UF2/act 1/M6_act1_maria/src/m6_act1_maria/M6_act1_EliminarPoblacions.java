5fe8dbd1a1b1cd120f01279b16871c8e6a3b3b99
maria <maria@LAPTOP-QQ1N2H54>
2020-12-21 20:29:02 +0100

    commit eliminar poblacions

--- M6/UF2/act 1/M6_act1_maria/src/m6_act1_maria/M6_act1_EliminarPoblacions.java
+++ M6/UF2/act 1/M6_act1_maria/src/m6_act1_maria/M6_act1_EliminarPoblacions.java
@@ -41,20 +41,12 @@ public class M6_act1_EliminarPoblacions extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jlCodiP = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        tfCPostal = new javax.swing.JTextField();
-        tfPoblacio = new javax.swing.JTextField();
         bEliminar = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTableEliminarP = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jlCodiP.setText("Codi Postal:");
-
-        jLabel2.setText("Nom poblacio:");
 
         bEliminar.setText("Eliminar");
         bEliminar.addActionListener(new java.awt.event.ActionListener() {
@@ -82,44 +74,25 @@ public class M6_act1_EliminarPoblacions extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(23, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jButton2))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(23, 23, 23)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jlCodiP)
-                                    .addComponent(jLabel2))
-                                .addGap(44, 44, 44)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(bEliminar)
-                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(tfCPostal)
-                                        .addComponent(tfPoblacio, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)))))))
-                .addContainerGap(49, Short.MAX_VALUE))
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(bEliminar))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 365, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jlCodiP)
-                    .addComponent(tfCPostal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(tfPoblacio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(bEliminar)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(bEliminar))
                 .addContainerGap())
         );
 
@@ -139,10 +112,36 @@ public class M6_act1_EliminarPoblacions extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
+    public void mostrarTabla() throws SQLException {
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Codi Postal");
+        model.addColumn("Població");
+        
+        jTableEliminarP.setModel(model); 
+        String[] datos  = new String[2];
+        String sql = "SELECT * FROM població";
+        Statement st;
+        try {
+            st = conection.createStatement();
+            
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()) {
+                datos[0] = rs.getString(1);
+                datos[1] = rs.getString(2);
+                model.addRow(datos);
+            }
+            jTableEliminarP.setModel(model);
+        } catch (Exception e) {
+        
+        }
+        
+    }
+    
     private void bEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bEliminarActionPerformed
         // TODO add your handling code here:
         int fila = jTableEliminarP.getSelectedRow();
-        String codigo = jTableEliminarP.getValueAt(fila, 1).toString();
+        String codigo = jTableEliminarP.getValueAt(fila, 0).toString();
         int cont = 0;
         Statement stmt;
         ResultSet rs;
@@ -166,7 +165,6 @@ public class M6_act1_EliminarPoblacions extends javax.swing.JFrame {
                         PreparedStatement pps = conection.prepareStatement("DELETE FROM població WHERE codiPostal = " + codigo + "");
                         pps.executeUpdate();
                         JOptionPane.showMessageDialog(null, "Dades esborrades");
-                        buidar();
                         mostrarTabla();
                     } catch (SQLException ex) {
                         Logger.getLogger(M6_act1_EliminarPoblacions.class.getName()).log(Level.SEVERE, null, ex);
@@ -174,7 +172,7 @@ public class M6_act1_EliminarPoblacions extends javax.swing.JFrame {
                 }
             }
             if(dialogButton == JOptionPane.NO_OPTION) {
-                  buidar();
+                  
             }
         try {
             mostrarTabla();
@@ -183,37 +181,7 @@ public class M6_act1_EliminarPoblacions extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_bEliminarActionPerformed
 
-    public void buidar(){
-        tfCPostal.setText("");
-        tfPoblacio.setText("");
-    }
     
-    public void mostrarTabla() throws SQLException {
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("Codi Postal");
-        model.addColumn("Població");
-        
-        jTableEliminarP.setModel(model); 
-        String[] datos  = new String[2];
-        String sql = "SELECT * FROM població";
-        Statement st;
-        System.out.println("hola2");
-        try {
-            st = conection.createStatement();
-            
-            ResultSet rs = st.executeQuery(sql);
-            while(rs.next()) {
-                datos[0] = rs.getString(1);
-                datos[1] = rs.getString(2);
-                model.addRow(datos);
-            }
-            System.out.println("hola");
-            jTableEliminarP.setModel(model);
-        } catch (Exception e) {
-        
-        }
-        
-    }
     /**
      * @param args the command line arguments
      */
@@ -258,12 +226,8 @@ public class M6_act1_EliminarPoblacions extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bEliminar;
     private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTableEliminarP;
-    private javax.swing.JLabel jlCodiP;
-    private javax.swing.JTextField tfCPostal;
-    private javax.swing.JTextField tfPoblacio;
     // End of variables declaration//GEN-END:variables
 }
