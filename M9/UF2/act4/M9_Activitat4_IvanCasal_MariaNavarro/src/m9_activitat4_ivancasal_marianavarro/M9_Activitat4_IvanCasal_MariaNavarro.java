94a7998c2551bdd64c3072de52d1823e41adfa59
maria <maria@DESKTOP-D34DJO6>
2021-01-26 18:16:12 +0100

    primer commit

---
+++ M9/UF2/act4/M9_Activitat4_IvanCasal_MariaNavarro/src/m9_activitat4_ivancasal_marianavarro/M9_Activitat4_IvanCasal_MariaNavarro.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m9_activitat4_ivancasal_marianavarro;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ *
+ * @author maria
+ */
+public class M9_Activitat4_IvanCasal_MariaNavarro {
+    
+    static class Client implements Callable<Integer> {
+        
+        private int totalProductos;
+        
+        public Client(int productos) {
+            totalProductos = productos;
+        }
+        
+        @Override
+        public Integer call() throws Exception {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        int CLIENTS = 50;
+        Client[] arrayC = new Client[CLIENTS];
+        
+        //Creem 50 clients
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(50);
+       
+        List<Client> llistaTasques = new ArrayList<Client>();
+        //3 segundos
+        //Thread.sleep(3000);
+        
+    }
+}
