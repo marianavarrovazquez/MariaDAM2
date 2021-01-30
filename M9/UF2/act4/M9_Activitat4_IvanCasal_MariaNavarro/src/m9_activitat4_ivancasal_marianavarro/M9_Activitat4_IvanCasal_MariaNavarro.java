dcd80398c5343fdea96c1e0bbd9b3b3e6ed8450a
maria <maria@DESKTOP-D34DJO6>
2021-01-30 22:05:00 +0100

    codi arreglat i acabat

--- M9/UF2/act4/M9_Activitat4_IvanCasal_MariaNavarro/src/m9_activitat4_ivancasal_marianavarro/M9_Activitat4_IvanCasal_MariaNavarro.java
+++ M9/UF2/act4/M9_Activitat4_IvanCasal_MariaNavarro/src/m9_activitat4_ivancasal_marianavarro/M9_Activitat4_IvanCasal_MariaNavarro.java
@@ -8,7 +8,6 @@ package m9_activitat4_ivancasal_marianavarro;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -19,36 +18,44 @@ public class M9_Activitat4_IvanCasal_MariaNavarro {
     
     static class Caja implements Runnable {
         //Numero de cliente y tiempo que puede tardar cada producto
-        private int[] temps = {2, 3, 4, 5, 6, 7, 8};
+        private int[] tiempo = {2, 3, 4, 5, 6, 7, 8};
         private int numClientes;
         
-        public Caja(int productos) {
-            numClientes = productos;
+        //Numero de productos aleatorios
+        private int productos = (int) (Math.random() * (5 - 1 + 1)) + 1;
+        
+        //tiempo que tardara cada producto
+        private int tiempoR = (int) (Math.random() * (6 - 0 + 1));
+        
+        
+        
+        public Caja(int numClientes) {
+            this.numClientes = numClientes;
+            //creamos los clientes y le indicamos los productos y el tiempo de cada uno
+            System.out.print("Creat Client " + numClientes + " amb " + productos + " articles (");
+                for (int k = 0; k < productos; k++){
+                    System.out.print(tiempo[tiempoR] + ", ");
+                }
+                System.out.print(")\n");
         }
         
         @Override
         public void run() {
-            //Numero de productos aleatorios
-            int articulos = (int) (Math.random() * (30 - 1 + 1)) + 1;
-            int tempsR = (int) (Math.random() * (6 - 0 + 1));
-            
-            System.out.println("Creat Client " + numClientes + " amb " + articulos + " articles");
             System.out.println("Client " + numClientes + " passa per caixa...");
-
-            for (int j = 1; j < articulos+1; j++) {
-
+            
+            for (int j = 1; j < productos + 1; j++) {
                 try {
                     //Tiempo que parara
-                    Thread.sleep(temps[tempsR] * 1000);
+                    Thread.sleep(tiempo[tiempoR] * 1000);
                 }catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                if (j == articulos) {
+                if (j == productos) {
                     //Si el cliente tiene todos los productos se va
-                    System.out.println("Client " + numClientes + " article " + j + "/" + articulos + " (" + temps[tempsR] + " segons)... FINALITZAT");
+                    System.out.println("Client " + numClientes + " article " + j + "/" + productos + " (" + tiempo[tiempoR] + " segons)... FINALITZAT");
                 }else{
                     //Si no los tiene sigue
-                    System.out.println("Client " + numClientes + " article " + j + "/" + articulos + " (" + temps[tempsR] + " segons)...");
+                    System.out.println("Client " + numClientes + " article " + j + "/" + productos + " (" + tiempo[tiempoR] + " segons)...");
                 }
             }
         }
@@ -61,9 +68,12 @@ public class M9_Activitat4_IvanCasal_MariaNavarro {
         //Numero de clientes
         for (int i = 1; i <= 2; i++) {
             Caja caja = new Caja(i);
-            executor.scheduleWithFixedDelay(caja, 0 , 3, TimeUnit.SECONDS);
+            executor.execute(caja);
+            
+           //SI PONEMOS ESTA LINEA I LA SIGUIENTE LOS CLIENTES SALEN EN ORDEN DESCENDIENTE  
+           //executor.scheduleWithFixedDelay(caja, 0 , 3, TimeUnit.SECONDS);
         }
-        executor.awaitTermination(20, TimeUnit.SECONDS);
+      //executor.awaitTermination(20, TimeUnit.SECONDS);
         executor.shutdown();
     }
 }
