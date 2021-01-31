c79ce22028a7734ac4c0a8231bc713e9e1620da9
maria <maria@DESKTOP-D34DJO6>
2021-01-31 23:35:02 +0100

    commit

---
+++ M9/UF2/act4/M9_Activitat4_IvanCasal_MariaNavarro/src/m9_activitat4_ivancasal_marianavarro/M9_act4_apartado2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m9_activitat4_ivancasal_marianavarro;
+
+/**
+ *
+ * @author maria e ivan
+ */
+public class M9_act4_apartado2 {
+    
+    static class Bombolla implements Runnable{
+        int[] arrayBombolla;
+       
+
+        @Override
+        public void run() {
+            
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
