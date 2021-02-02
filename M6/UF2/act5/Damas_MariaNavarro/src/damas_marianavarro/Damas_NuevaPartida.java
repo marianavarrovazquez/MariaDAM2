bed986d126e546b71e3161b41f2937e6f8f9aa85
maria <maria@DESKTOP-D34DJO6>
2021-02-02 21:07:18 +0100

    commit

--- M6/UF2/act5/Damas_MariaNavarro/src/damas_marianavarro/Damas_NuevaPartida.java
+++ M6/UF2/act5/Damas_MariaNavarro/src/damas_marianavarro/Damas_NuevaPartida.java
@@ -5,14 +5,9 @@
  */
 package damas_marianavarro;
 
-
-import entity.Moviment;
-import entity.Partida;
 import javax.swing.JOptionPane;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 /**
  *
@@ -21,21 +16,20 @@ import org.hibernate.Transaction;
 
 public class Damas_NuevaPartida extends javax.swing.JFrame {
 
-    boolean jugaX = true;
-    boolean jugaO = false;
+    boolean jugaX = false;
+    boolean jugaO = true;
     int filaOrigen = -1;
     int columnaOrigen = -1;
-    int filaDestino = -1;
-    int columnaDestino = -1;
+    int filaValida = -1;
+    int columnaValida = -1;
     
     static Session session;
-    static Partida partida;
-    static Moviment moviment;
+//    static Partida partida;
+//   static Moviment moviment;
 
     public Damas_NuevaPartida() {
         initComponents();
-        partida = new Partida("");
-        nPartida("?");
+  //      nPartida("1/2");
     }
 
     @SuppressWarnings("unchecked")
@@ -43,73 +37,65 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        taula = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        taula = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        taula.setBackground(new java.awt.Color(240, 240, 240));
-        taula.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        taula.setFont(new java.awt.Font("Arial", 0, 28)); // NOI18N
+        jButton1.setText("Sortir");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        taula.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         taula.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {"   X", null, "   X", null, "   X", null, "   X", null},
+                {"   X", null, "   X", "", "   X", null, "   X", null},
                 {null, "   X", null, "   X", null, "   X", null, "   X"},
                 {null, null, null, null, null, null, null, null},
                 {null, null, null, null, null, null, null, null},
                 {null, null, null, null, null, null, null, null},
                 {null, null, null, null, null, null, null, null},
-                {"   O", null, "   O", "", "   O", null, "   O", null},
+                {"   O", null, "   O", null, "   O", null, "   O", null},
                 {null, "   O", null, "   O", null, "   O", null, "   O"}
             },
             new String [] {
                 "", "", "", "", "", "", "", ""
             }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                true, true, false, true, true, true, true, true
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        taula.setFocusable(false);
-        taula.setGridColor(new java.awt.Color(0, 0, 0));
+        ));
         taula.setRowHeight(50);
-        taula.setSelectionBackground(new java.awt.Color(0, 0, 0));
-        taula.setSelectionForeground(new java.awt.Color(0, 0, 0));
         taula.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 taulaMouseClicked(evt);
             }
         });
-        jScrollPane1.setViewportView(taula);
-
-        jButton1.setText("Sortir");
+        jScrollPane2.setViewportView(taula);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 543, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(472, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jScrollPane2)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(21, 21, 21)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
                 .addComponent(jButton1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -128,28 +114,34 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     private void taulaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_taulaMouseClicked
         // TODO add your handling code here:
         int fila = obtenirFilaClicada();
         int columna = obtenirColumnaClicada();
         
         if(noHiHaOrigen()) {
-            if(jugaX && EsX(fila, columna)){
-                ActualitzaNouOrigen(fila,columna);
-            } else if(jugaO && EsO(fila, columna)){
-                ActualitzaNouOrigen(fila,columna);
+            if (jugaX && EsX(fila, columna)) {
+                ActualitzaNouOrigen(fila, columna);
+            } else if (jugaO && EsO(fila, columna)) { 
+                ActualitzaNouOrigen(fila, columna);
             } else {
                 mostraError();
             }
         } else {
-            if(movimentValid(fila, columna)){
-                if(esBuit(fila, columna)||OcupatContrari(fila, columna)){
-                    mou(fila,columna);
-                } else if(OcupatPropi(fila,columna)){
-                    ActualitzaNouOrigen(fila,columna);
-                }
+            if (movimentValid(fila, columna)) {
+                if (esBuit(fila, columna) || OcupatContrari(fila, columna)) {
+                    mou(fila, columna);
+                    ganador(fila, columna);
+                } else if (OcupatPropi(fila, columna)) {
+                    ActualitzaNouOrigen(fila, columna);
+                } 
             } else {
-                mostraErrorMoviment();
+                    mostraErrorMoviment();
             }
         }
     }//GEN-LAST:event_taulaMouseClicked
@@ -163,11 +155,11 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
     }
 
     public boolean noHiHaOrigen(){
-        boolean nOrigen = false;
+        boolean noHayOrigen = false;
         if(filaOrigen == -1 || columnaOrigen == -1){
-            nOrigen = true;
+            noHayOrigen = true;
         }
-        return nOrigen;
+        return noHayOrigen;
     }
 
     private boolean EsX(int fila, int columna) {
@@ -192,27 +184,24 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
     }
 
     public void mostraError(){
-        JOptionPane.showMessageDialog(null, "Error!!", "Juego damas", 
-                JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Error!!", "Damas", JOptionPane.ERROR_MESSAGE);
         filaOrigen = -1;
         columnaOrigen = -1;
     }
 
     public boolean movimentValid(int fila, int columna) {
-        boolean esMovimentValid = false;
-        filaDestino = fila;
-        columnaDestino = columna;
-        int calculaFila =  filaDestino - filaOrigen;
-        int calculaColumna = columnaDestino - columnaOrigen;
+        boolean valido = false;
+        filaValida = fila;
+        columnaValida = columna;
+        int calculaF =  filaValida - filaOrigen;
+        int calculaC = columnaValida - columnaOrigen;
         
-        if(jugaO && (calculaFila == -1) && 
-                ((calculaColumna == 1) || (calculaColumna ==  -1))){
-            esMovimentValid = true;
-        } else if (jugaX && (calculaFila == 1) && 
-                ((calculaColumna == 1) || (calculaColumna ==  -1))) {
-            esMovimentValid = true;
+        if(jugaO && (calculaF == -1) && ((calculaC == 1) || (calculaC ==  -1))){
+            valido = true;
+        } else if (jugaX && (calculaF == 1) && ((calculaC == 1) || (calculaC ==  -1))) {
+            valido = true;
         }
-        return esMovimentValid;
+        return valido;
     }
 
     public boolean esBuit(int fila, int columna) {
@@ -224,25 +213,23 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
     }
 
     public boolean OcupatContrari(int fila, int columna) {
-        boolean isOcupatContrari = false;
-        if((jugaX == true && EsO(fila, columna) == true)
-                || (jugaO == true && EsX(fila, columna) == true)){
-            isOcupatContrari = true;
+        boolean contrari = false;
+        if((jugaX == true && EsO(fila, columna) == true) || (jugaO == true && EsX(fila, columna) == true)){
+            contrari = true;
         }
-    
-        return isOcupatContrari;
+        return contrari;
     }
 
     public void mou(int fila, int columna) {
-        int checker = 0;
-        
+        int contador = 0;
+    //    nMovimiento(filaOrigen, fila, columnaOrigen, columna);
         taula.setValueAt(null, filaOrigen, columnaOrigen);
         if (jugaO) {
             taula.setValueAt("O", fila, columna);
             filaOrigen = -1;
             columnaOrigen = -1;
-            if(checker == 0) {
-                checker = 1;
+            if(contador == 0) {
+                contador = 1;
                 jugaO = false;
                 jugaX = true;
             }
@@ -250,26 +237,25 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
             taula.setValueAt("X", fila, columna);
             filaOrigen = -1;
             columnaOrigen = -1;
-            if(checker == 0) {
-                checker = 1;
+            if(contador == 0) {
+                contador = 1;
                 jugaO = true;
                 jugaX = false;
             }
-            checker = 0;
+            contador = 0;
         }
     }
 
     public boolean OcupatPropi(int fila, int columna) {
-        boolean isOcupatPropi = false;
+        boolean ocupado = false;
         if((jugaX == true && EsX(fila, columna) == true) || (jugaO == true && EsO(fila, columna) == true)){
-            isOcupatPropi  = true;
+            ocupado  = true;
         }
-        
-        return isOcupatPropi;
+        return ocupado;
     }
 
     public void mostraErrorMoviment() {
-        JOptionPane.showMessageDialog(null, "error al moure la fitxa", "Juego damas", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Error al moure la fitxa", "Damas", JOptionPane.ERROR_MESSAGE);
         filaOrigen = -1;
         columnaOrigen = -1;
     }
@@ -282,7 +268,7 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
             Damas_P1 damas = new Damas_P1();
             damas.setVisible(true);
             dispose();
-            nuevaPartida("X");
+            nPartida("X");
         } else if (EsO(fila, columna) && fila == 0) {
             jugaX = false; 
             jugaO = false;
@@ -291,16 +277,16 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
             Damas_P1 damas = new Damas_P1();
             damas.setVisible(true);
             dispose();
-            nuevaPartida("O");
+            nPartida("O");
         }
     }
     
     private void nPartida(String ganador) {
-        partida.setGanador(ganador);
+//        partida.setGanador(ganador);
         try {
-            session = NewHibernateUtil.getSessionFactory().openSession();
+     //       session = NewHibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
-            session.saveOrUpdate(partida);
+//            session.saveOrUpdate(partida);
             session.getTransaction().commit();
         
         } catch (HibernateException e) {
@@ -309,7 +295,7 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
             session.close();
         }
     }
-    
+  /*
     public static void nMovimiento(int columnaOrigen, int columnaDestino, int filaOrigen, int filaDestino) {
         
         moviment = new Moviment(partida, columnaOrigen, columnaDestino, filaOrigen, filaDestino);
@@ -321,7 +307,7 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
         moviment.setFilaDestino(filaDestino);
         
         try {
-            session = NewHibernateUtil.getSessionFactory().openSession();
+    //        session = NewHibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
             session.persist(moviment);
             session.getTransaction().commit();
@@ -333,7 +319,7 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
         }
         
     }
-    
+*/
     /**
      * @param args the command line arguments
      */
@@ -372,8 +358,10 @@ public class Damas_NuevaPartida extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable taula;
     // End of variables declaration//GEN-END:variables
+
+    
 
 }
