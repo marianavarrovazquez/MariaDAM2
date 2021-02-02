7862056412168d177ce2b8237c9656105de4fc7c
maria <maria@DESKTOP-D34DJO6>
2021-02-02 15:23:27 +0100

    commit

--- M9/UF2/act4/M9_Activitat4_IvanCasal_MariaNavarro/src/m9_activitat4_ivancasal_marianavarro/M9_act4_apartado2.java
+++ M9/UF2/act4/M9_Activitat4_IvanCasal_MariaNavarro/src/m9_activitat4_ivancasal_marianavarro/M9_act4_apartado2.java
@@ -5,6 +5,8 @@
  */
 package m9_activitat4_ivancasal_marianavarro;
 
+import java.util.Arrays;
+
 /**
  *
  * @author maria e ivan
@@ -13,15 +15,38 @@ public class M9_act4_apartado2 {
     
     static class Bombolla implements Runnable{
         int[] arrayBombolla;
+        int parts;
        
+        public Bombolla(int[] arrayBombolla, int parts) {
+            this.arrayBombolla = arrayBombolla;
+            this.parts = parts;
+        }
 
         @Override
         public void run() {
+            int[] arrayBombolla2;
+            int parts2;
             
+            for (int i = 0; i < arrayBombolla.length; i++) {
+                for (int j = 0; j < arrayBombolla.length; j++) {
+                    if(arrayBombolla[j] > arrayBombolla[j+1]) {
+                        arrayBombolla2 = arrayBombolla.clone();
+                        parts2 = arrayBombolla[j];
+                        arrayBombolla[j] = arrayBombolla[j+1];
+                        arrayBombolla[j+1] = parts2;
+                        System.out.println("Parte " + (this.parts + 1) + " era " + Arrays.toString(arrayBombolla2) + " ahora es " + Arrays.toString(arrayBombolla));
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(arrayBombolla));
         }
+        
     }
     
+    static int[] arrayParts = {0, 0, 0, 0};
+    
     public static void main(String[] args) {
+        
         
     }
 }
