2d6d6cce95510a22ee1ea2cbc669dc8452b19fb6
maria <maria@DESKTOP-D34DJO6>
2021-02-25 21:13:59 +0100

    commit

---
+++ M3/M3_act5/src/m3_act5/M3_act5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m3_act5;
+
+/**
+ *
+ * @author maria
+ */
+public class M3_act5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
