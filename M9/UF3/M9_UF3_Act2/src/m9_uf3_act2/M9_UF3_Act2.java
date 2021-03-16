67c9006f4128d322d24f15aff4ce6940103e978a
maria <maria@DESKTOP-D34DJO6>
2021-03-16 20:13:13 +0100

    commit

--- M9/UF3/M9_UF3_Act2/src/m9_uf3_act2/M9_UF3_Act2.java
+++ M9/UF3/M9_UF3_Act2/src/m9_uf3_act2/M9_UF3_Act2.java
@@ -15,53 +15,35 @@ import java.util.Scanner;
  */
 public class M9_UF3_Act2 {
 	
-	public static void main (String[] args) {
-		String urlTeclado;
-                int port;
-                URL url = null;
-                Scanner scan = new Scanner(System.in);
-		
-		try {
-			System.out.println("Escriu la url: ");
-                        urlTeclado = scan.nextLine();
-                        
-                        System.out.println("Escriu el port: ");
-                        port = scan.nextInt();
-                        
-			url = new URL("http", urlTeclado, port, "");
-//			Visualitzar (url);
-			
-		} catch (MalformedURLException e) {e.printStackTrace(); }
-                
-                BufferedReader in;
-                
-                try{
-                    InputStream inputStream = url.openStream();
-			in = new BufferedReader(new InputStreamReader(inputStream));
-			
-			String inputLine;
-			
-			while ((inputLine = in.readLine()) != null)
-				System.out.println(inputLine);
-			in.close();
-			
-		} catch (IOException e) {e.printStackTrace(); }
+    public static void main (String[] args) {
+        String urlTeclado;
+        int port;
+        URL url = null;
+        Scanner scan = new Scanner(System.in);
 
-		
-	}
-	
-//	private static void Visualitzar(URL url) {
-//		
-//		System.out.println("\tURL complerta: "+url.toString());
-//		System.out.println("\tgetProtocol: "+url.getProtocol());
-//		System.out.println("\tgetHost: "+url.getHost());
-//		System.out.println("\tgetPort: "+url.getPort());
-//		System.out.println("\tgetFile: "+url.getFile());
-//		System.out.println("\tgetUserInfo: "+url.getUserInfo());
-//		System.out.println("\tgetPath: "+url.getPath());
-//		System.out.println("\tgetAuthority: "+url.getAuthority());
-//		System.out.println("\tgetQuery: "+url.getQuery());
-//		System.out.println("=====================================================");
-//	}
+        try {
+                System.out.println("Escriu la url: ");
+                urlTeclado = scan.nextLine();
 
+                System.out.println("Escriu el port: ");
+                port = scan.nextInt();
+
+                url = new URL("http", urlTeclado, port, "");
+
+        } catch (MalformedURLException e) {e.printStackTrace(); }
+
+        BufferedReader in;
+
+        try{
+            InputStream inputStream = url.openStream();
+            in = new BufferedReader(new InputStreamReader(inputStream));
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null)
+                    System.out.println(inputLine);
+            in.close();
+
+        } catch (IOException e) {e.printStackTrace(); }
+    }
 }
