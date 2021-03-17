7c2bd0bbc1019e40db37b9a7cdce1eeccda8ebd5
maria <maria@DESKTOP-D34DJO6>
2021-03-17 21:29:58 +0100

    commit

--- M3/M3_NF2_Act2/src/m3_nf2_act2/Activitat2_MariaNavarro.java
+++ M3/M3_NF2_Act2/src/m3_nf2_act2/Activitat2_MariaNavarro.java
@@ -14,24 +14,25 @@ public class Activitat2_MariaNavarro {
     
     private static void calcula(int fila, int pedres) {
         if(fila == pedres) {
-            System.out.println("Estem a la fila " + fila + " i queden" + pedres + "pedres");
+            pedres = pedres - fila;
+            System.out.println("Si son iguals: ");
+            System.out.println("Estem a la fila " + fila + " i queden " + pedres + " pedres\n");
         } else if (fila > pedres) {
             fila = fila - 1;
-            System.out.println("Estem a la fila " + fila + " i queden" + pedres + "pedres");
+            System.out.println("Si fila es mes gran que pedres: ");
+            System.out.println("Estem a la fila " + fila + " i queden " + pedres + " pedres\n");
         } else {
-            fila = fila + 1;
             pedres = pedres - fila;
+            fila = fila + 1;
             calcula(fila, pedres);
-            System.out.println("Estem a la fila " + fila + " i queden" + pedres + "pedres");
+            System.out.println("Si fila es mes petit que pedres: ");
+            System.out.println("Estem a la fila " + fila + " i queden " + pedres + " pedres\n");
         }
     }
    
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-        int fila, pedres = 5;
+        int fila, pedres = 10;
         
         calcula(1, pedres);
         
