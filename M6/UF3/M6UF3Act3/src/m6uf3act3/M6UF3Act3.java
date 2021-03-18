14ebd676f86e075eccc3f9ddce18926ade730bbf
maria <maria@DESKTOP-D34DJO6>
2021-03-18 17:09:20 +0100

    commit

---
+++ M6/UF3/M6UF3Act3/src/m6uf3act3/M6UF3Act3.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m6uf3act3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.xmldb.api.*;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.*;
+
+/**
+ *
+ * @author maria
+ */
+public class M6UF3Act3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws XMLDBException {
+        // TODO code application logic here
+        String driver = "org.exist.xmldb.DatabaseImpl";
+        Collection col = null;
+        String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db";
+        String usu = "admin";
+        String usuPass = "alumne";
+       
+        try {
+            Class cl = Class.forName(driver);
+            Database database = (Database) cl.newInstance();
+            DatabaseManager.registerDatabase(database);
+        } catch(Exception e) {
+            System.out.println("Error en iniciar eXistdb" + e);
+        }
+        
+        System.out.println("Escriu un departament");
+        String s = null;
+        
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            s = in.readLine();
+        } catch (IOException e) {
+            System.out.println("Error en llegir");
+            e.printStackTrace();
+        }
+        int dep = Integer.parseInt(s);
+       
+        col = DatabaseManager.getCollection(URI, usu, usuPass);
+        if (col == null)
+            System.out.println("la coleccio no existeix");
+       
+        XPathQueryService servei =
+                (XPathQueryService) col.getService("XPathQueryService", "1.0");
+        ResourceSet result =
+                servei.query("for $em in /EMPLEADOS/EMP_ROW[DEPT_NO=20] return $em");
+       
+        ResourceIterator i;
+        i = result.getIterator();
+        if (!i.hasMoreResources())
+            System.out.println("la consulta no retorna res");
+        while (i.hasMoreResources()) {
+            Resource r = i.nextResource();
+            System.out.println("" + (String)r.getContent());
+        }
+        col.close();
+    }
+   
+}
