05253433b978555ace65f72d6c6867d71cd437b4
maria <maria@DESKTOP-D34DJO6>
2021-04-29 13:56:55 +0200

    commit Funciona el primer link

---
+++ M2/M2_Act4/src/java/act04/ColorBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package act04;
+
+/**
+ *
+ * @author maria
+ */
+public class ColorBean {
+    private String foregroundColor = "black";
+    private String backgroundColor = "white";
+
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+}
