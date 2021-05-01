b2ac5bff43628734cc8b1055cab08904efb26800
maria <maria@DESKTOP-D34DJO6>
2021-05-01 23:05:40 +0200

    commit

--- M9/UF3/M9_UF3_Act10/M9_Act10/src/m9_act10/ServerChat.java
+++ M9/UF3/M9_UF3_Act10/M9_Act10/src/m9_act10/ServerChat.java
@@ -15,119 +15,155 @@ import java.util.Scanner;
  */
 public class ServerChat implements Runnable{
     
-    static Socket client;
-    static ServerSocket server;
+    Socket client;
+    ServerSocket server;
     static int numClients;
-    String cadena = "";
-    static int port = 60000;
-    
-    //Arrays globals
-    static ServerChat[] arrayRunnable;
-    static Thread[] arrayThread;
-    static Socket[] clients;
+    static String cadena = "";
+    static int numClient = 1;
+    String name;
+            
+    Socket[] clientSocket;
 
-    public ServerChat(Socket client, ServerSocket server, Socket[] clients) {
+    public ServerChat(Socket client, ServerSocket server, Socket[] clientSocket) {
         this.client = client;
         this.server = server;
         this.numClients++;
-        this.clients = clients;
+        this.clientSocket = clientSocket;
     }
     	
     public static void main (String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        server = new ServerSocket(port);
+        int port = 60000;
+    
+        ServerSocket serverSocket = new ServerSocket(port);
 
         System.out.print("Introdueix el numero de clients que pots atendre: ");
         int numClients = scan.nextInt();
 
-        arrayRunnable = new ServerChat[numClients];
-        arrayThread = new Thread[numClients];
-        clients = new Socket[numClients];
+        ServerChat[] arrayRunnable = new ServerChat[numClients];
+        Thread[] arrayThread = new Thread[numClients];
+        Socket[] clientSocket = new Socket[numClients];
         
         for (int i = 0; i < arrayRunnable.length; i++) {
             boolean bolean = true;
             boolean desconectar = false;
-            client = null;
-            
-            System.out.println("Esperant connexió... ");
+            Socket client2 = null;
             
             try{
-                client = server.accept();
+                client2 = serverSocket.accept();
+                numClient = i;
             } catch (SocketException e) {
                 bolean = false;
             }
             
-            for (int j = 0; j < clients.length; j++) {
-                if (clients[i] == null && desconectar == false) {
-                    clients[i] = client;
+            for (int j = 0; j < clientSocket.length; j++) {
+                if (clientSocket[i] == null && desconectar == false) {
+                    clientSocket[i] = client2;
                     desconectar = true;
                 }
             }
             
             if(bolean) {
-                arrayRunnable[i] = new ServerChat(client, server, clients);
+                arrayRunnable[i] = new ServerChat(client2, serverSocket, clientSocket);
                 arrayThread[i] = new Thread(arrayRunnable[i]);
                 arrayThread[i].start();
             }
         }
+        
+        serverSocket.close();
     }
 
     @Override
     public void run() {
-        boolean stop = false;
-        PrintWriter fsortida = null;
-        BufferedReader fentrada = null;
+        boolean desconectar = false;
+        String cadenaName = cadena.substring(7, cadena.length()).toString();
         
-        while (!stop) {
+        while (!desconectar) {
             try {           
-                System.out.println("Client " + this.numClients + " connectat... ");
+                PrintWriter fsortida = null;
+                BufferedReader fentrada = null;
+                System.out.println("Client " + numClient + " connectat... ");
                 try {
                     fsortida = new PrintWriter(client.getOutputStream(), true);
                     fentrada = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
-                    
-                    if ((cadena = fentrada.readLine()) != null) {
-                        System.out.println("Nom Client (" + this.numClients + "): " + cadena);
+                    fsortida.println("Connexio client " + numClient + " feta...");
+                    if ((cadena = fentrada.readLine()) != null && cadena.startsWith("//name ")) {
+                        String auxName = cadena.replace("//name", "");
+                        for (int i = 1; i < clientSocket.length; i++) {
+                            if (numClient != i) {
+                                if (name == null || !name.equals(auxName)) {
+                                    name = auxName;
+                                }
+                            }
+                        }
+                        
+                        if (clientSocket.length <= 1){
+                            name = auxName;
+                        }
+                        
+                        System.out.println("Nom Client (" + numClient + "): " + cadenaName);
                     }
                 } catch (SocketException e) { 
-                    stop = true;
+                    desconectar = true;
                 }
                 
-                while(!stop) {
+                while(!desconectar) {
                     try {
                         cadena = fentrada.readLine();
                     } catch (SocketException e) { 
-                        stop = true;
+                        desconectar = true;
                     }
                     
-                    if (cadena.equals("//sortir")){
-//                        System.out.println("Connexió " + this.registre + " client tancada... ");
-                        stop = true;
+                    if (cadena == null && cadena.equals("//sortir")){
+                        System.out.println("Connexió " + name + " client tancada... ");
+                        desconectar = true;
                     }
+                    
+                    if (!desconectar) {
+                        fsortida.println(cadena);
+
+                        if (cadena != null) {
+                            for (int i = 0; i < clientSocket.length; i++) {
+                                if (clientSocket[i] != null) {
+                                    fsortida = new PrintWriter(this.clientSocket[i].getOutputStream(), true);
+                                    fsortida.println(cadena);
+                                }
+                            }
+                            System.out.println("Rebent: " + cadena);
+                        }
+                    }
+                    
                 }
 
-                while ((cadena = fentrada.readLine()) != "") {                
-                    if (cadena.startsWith("//name ")) {
-                        this.registre = cadena.substring(7, cadena.length()).toString();
-                        System.out.println("Client " + this.registre + " connectat... ");                    
-                    } else if (!cadena.equals("//sortir")) {
-    //                    fsortida.println(cadena);
-                        System.out.println("Rebent: " + cadena);
-                        //Retornem missatges
-                        fsortida.println(this.registre + ": " + cadena);
-                    } else if (cadena.equals("//sortir")){
-                        System.out.println("Connexió " + this.registre + " client tancada... ");
-                        break;
-                    }
+//                while ((cadena = fentrada.readLine()) != "") {                
+//                    if (cadena.startsWith("//name ")) {
+//                        this.registre = cadena.substring(7, cadena.length()).toString();
+//                        System.out.println("Client " + this.registre + " connectat... ");                    
+//                    } else if (!cadena.equals("//sortir")) {
+//    //                    fsortida.println(cadena);
+//                        System.out.println("Rebent: " + cadena);
+//                        //Retornem missatges
+//                        fsortida.println(this.registre + ": " + cadena);
+//                    } else if (cadena.equals("//sortir")){
+//                        System.out.println("Connexió " + this.registre + " client tancada... ");
+//                        break;
+//                    }
+//                }
+                 try {
+                    fentrada.close();
+                    fsortida.close();
+                } catch (NullPointerException e) {
+                    // TODO: handle exception
                 }
-                fentrada.close();
-                fsortida.close();
+
                 this.client.close();
                 this.server.close();
+
             } catch (IOException e) {
+                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
-        
+
     }
 }
-
